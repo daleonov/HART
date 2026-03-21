76d6f26974e69de26b9bd3871cb8345351ec3c4e
Daniel Leonov <daleonov@yandex.ru>
2026-03-21 07:09:17 +0300

    Tests: Add more tests for MatcherFunction

--- tests/test_matchers.cpp
+++ tests/test_matchers.cpp
@@ -42,11 +42,29 @@ HART_TEST ("Matcher function - For output buffer")
 {
     using AudioBuffer = hart::AudioBuffer<float>;
 
-    processAudioWith (GainDb())
+    processAudioWith (GainDb (0_dB))
         .withLabel ("Creating MatcherFunction explicitly")
         .withInputSignal (SineWave())
-        .expectTrue (MatcherFunction ([] (const AudioBuffer&) { return true; }))
-        .expectFalse (MatcherFunction ([] (const AudioBuffer&) { return false; }))
+        .expectTrue (MatcherFunction ([] (const AudioBuffer&) { return true; }, "Always true"))
+        .expectFalse (MatcherFunction ([] (const AudioBuffer&) { return false; }, "Always false"))
+        .process();
+
+    auto peaksAboveUnity = MatcherFunction (
+        [] (const AudioBuffer& output) { return output.getMagnitude (0, output.getNumFrames()) > 1.0f; },
+        "Peaks above unity"
+        );
+
+    processAudioWith (GainDb (0_dB))
+        .withLabel ("Using a named object")
+        .withInputSignal (SineWave() >> GainDb (3_dB))
+        .expectTrue (std::move (peaksAboveUnity))
+        .process();
+
+    processAudioWith (GainDb (0_dB))
+        .withLabel ("Creating MatcherFunction implicitly")
+        .withInputSignal (SineWave())
+        .expectTrue ([] (const AudioBuffer&) { return true; }, "Always true")
+        .expectFalse ([] (const AudioBuffer&) { return false; }, "Always false")
         .process();
 }
 
@@ -54,10 +72,17 @@ HART_TEST ("Matcher function - For input and output buffers")
 {
     using AudioBuffer = hart::AudioBuffer<float>;
 
-    processAudioWith (GainDb())
+    processAudioWith (GainDb (0_dB))
         .withLabel ("Creating MatcherFunction explicitly")
         .withInputSignal (SineWave())
-        .expectTrue (MatcherFunction ([] (const AudioBuffer&, const AudioBuffer&) { return true; }))
-        .expectFalse (MatcherFunction ([] (const AudioBuffer&, const AudioBuffer&) { return false; }))
+        .expectTrue (MatcherFunction ([] (const AudioBuffer&, const AudioBuffer&) { return true; }, "Always true"))
+        .expectFalse (MatcherFunction ([] (const AudioBuffer&, const AudioBuffer&) { return false; }, "Always false"))
+        .process();
+
+    processAudioWith (GainDb (0_dB))
+        .withLabel ("Creating MatcherFunction implicitly")
+        .withInputSignal (SineWave())
+        .expectTrue ([] (const AudioBuffer&, const AudioBuffer&) { return true; }, "Always true")
+        .expectFalse ([] (const AudioBuffer&, const AudioBuffer&) { return false; }, "Always false")
         .process();
 }
