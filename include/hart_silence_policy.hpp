77e5767a7b41f354f6c4cbc0f2b29767331d500a
Daniel Leonov <daleonov@yandex.ru>
2026-04-01 19:11:06 +0300

    Docs: Add documentation to the SilencePolicy enum

--- include/hart_silence_policy.hpp
+++ include/hart_silence_policy.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
-// TODO: Add docs to this enum
-
 namespace hart
 {
 
-///@ingroup Utilities
+/// @brief Defines how silence in various algorithms
+/// @details Exact meaning of each option depends on the algorithm,
+/// so refer to the docs of each specific algorithm or class for details. 
+/// @ingroup Utilities
 enum class SilencePolicy
 {
     strict,
