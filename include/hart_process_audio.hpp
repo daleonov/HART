b837f8ee45b2d78dc6e03f793701a99bf7794a30
Daniel Leonov <daleonov@yandex.ru>
2026-04-29 22:02:14 +0300

    Runner: Add querying whether the input signal supports the requested sample rare and number of channels before rendering audio

--- include/hart_process_audio.hpp
+++ include/hart_process_audio.hpp
@@ -649,6 +649,12 @@ public:
         if (m_inputSignal == nullptr)
             m_inputSignal = std::move (hart::make_unique<Silence<SampleType>>());
 
+        if (! m_inputSignal->supportsSampleRate (m_sampleRateHz))
+            HART_THROW_OR_RETURN (hart::SampleRateError, "Input signal does not support requested sample rate", std::move (m_processor));
+
+        if (! m_inputSignal->supportsNumChannels (m_numInputChannels))
+            HART_THROW_OR_RETURN (hart::ChannelLayoutError, "Input signal does not support requested number of channels", std::move (m_processor));
+
         if (m_signalPreparationBeforeWarmUp == Preparation::reset || m_signalPreparationBeforeWarmUp == Preparation::resetAndPrepare)
             m_inputSignal->resetWithDSPChain();
 
