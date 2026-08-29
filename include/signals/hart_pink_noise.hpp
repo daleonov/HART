79c0e21856d6a3a5ff8417a509e823c476c4fc06
Daniel Leonov <daleonov@yandex.ru>
2026-08-29 07:42:49 +0300

    Docs: Add class documentation for the PinkNoise Signal

--- include/signals/hart_pink_noise.hpp
+++ include/signals/hart_pink_noise.hpp
@@ -9,16 +9,72 @@
 #include "signals/hart_signal.hpp"
 #include "hart_utils.hpp"  // makeRandomSeeds()
 
-// TODO: Document it
-
 namespace hart
 {
 
+/// @brief Produces deterministic pink noise
+/// @details Calibrated to -12 dB RMS, with sample peaks being around unity gain.
+///
+/// This Signal is constructed in time domain, with high CPU efficiency, and
+/// quite accurate, but not perfect, frequency response. You may also use
+/// `Spectrum::colouredNoise()`, combined with `Spectrum::toSignal()`, to get a
+/// finite signal source with ideal frequency response, if you want the highest
+/// frequency responce accuracy, and don't mind its finite nature, and substantially
+/// higher CPU cost.
+///
+/// This Signal is implemented around Paul Kellet's pinking filter, described in a
+/// [DSP generation of Pink (1/f) Noise](https://www.firstpr.com.au/dsp/pink-noise/)
+/// article. The general idea is a cascade of one-pole filters, hand-tuned to
+/// have a frequency response within a claimed +/- 0.05 dB accuracy, and feeding
+/// white noise through it, to achieve a surprisingly accurate pink noise signal.
+///
+/// This class is a pretty faithful implementation of Paul Kellet's "instrumentation
+/// grade" version of the filter, with some extra calibration and measurements to make
+/// it fit HART standards. Below listed are some measurements of this Signal's output.
+/// You can see more details about this specific Signal implementation in my design
+/// notes in the `Scripts/PinkNoise Filter.ipynb` notebook, shipped with this repo.
+///
+/// Contains energy down to DC frequency, and has a pretty accurate frequency response
+/// above 9.2 Hz at 44.1 kHz, according to the author of the original filter. However,
+/// it was designed for 44.1 kHz sample rate, and while it's still very accurate at
+/// other sample rates, it's best to use it only up to 96 kHz included, as the lower
+/// end frequency response at very high sample rates (such as 192 kHz) is no longer
+/// as accurate in the audible frequency range (see the chart below).
+///
+/// **Maximum spectral-shape error above 20 Hz after optimal
+/// constant gain alignment at common sample rates:**
+///
+/// Sample rate, Hz | Max error, dB
+/// --------------- | --------------
+///           44100 |         0.029
+///           48000 |         0.029
+///           88200 |         0.030
+///           96000 |         0.035
+///          176400 |         0.460
+///          192000 |         0.562
+///
+/// **Output levels**
+///
+/// Output RMS calibrated to -12 dB. Measured output sample peaks
+/// (see `PinkNoise Filter.ipynb` for details): median: -2.37 dB, p95: -0.23 dB.
+///
+/// For multi-channel set-ups, each channel will produce a unique noise instance.
+///
+/// @warning It is not recommended to use it with very high sample rates, such
+/// as 192 kHz or higher - see the descritpion for details. However, it's still
+/// usable at those very high rates, with a caveat of less accurate response at
+/// the lower end of audible frequency range.
+/// @tparam SampleType 
+/// @ingroup Signals
 template <typename SampleType>
 class PinkNoise :
     public Signal<SampleType, PinkNoise<SampleType>>
 {
 public:
+
+    /// @brief Constructs a PinkNoise Signal instance
+    /// @param randomSeed RNG seed value for generating noise. Per-channel RNG seeds will
+    /// be derived from this value. Defaults to global seed value, controlled via CLI.
     PinkNoise (uint_fast32_t randomSeed = CLIConfig::getInstance().getRandomSeed()) :
         m_randomSeed (randomSeed)
     {
