b0c65debb608b5fa0507ba4c990984ba8831bb8d
Daniel Leonov <daleonov@yandex.ru>
2026-09-03 04:34:54 +0300

    Tests: Add "test_basics.cpp" for documenting basic features

---
+++ tests/test_basics.cpp
@@ -0,0 +1,11 @@
+#include "hart.hpp"
+
+HART_TEST ("Basics - Simple one-shot test case")
+{
+    HART_EXPECT_EQ (2 + 2, 4);
+}
+
+HART_TEST_WITH_TAGS ("Basics - One-shot test case with tags", "[some-tag][some-other-tag]")
+{
+    HART_EXPECT_EQ (2 + 2, 4);
+}
